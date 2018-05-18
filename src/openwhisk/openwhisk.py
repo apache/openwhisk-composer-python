669e3ed8b54464075ef0ae68316331a553514090
Lionel Villard <villard@us.ibm.com>
2018-05-18 12:18:59 -0400

    more diags

--- src/openwhisk/openwhisk.py
+++ src/openwhisk/openwhisk.py
@@ -59,14 +59,13 @@ class Client:
         headers = { 'Authorization': self.auth_header(), 'Content-Type': 'application/json' }
         verify = not self.options['ignore_certs']
 
-        print("APIKEY:", self.options['api_key'])
-
+        print('URL:', url)
         resp = requests.request(method, url, params=params, data=payload, headers=headers, verify=verify)
 
         if resp.status_code >= 400:
             print(resp)
             print(resp.status_code)
-            print(resp.text())
+            print(resp.text)
             # we turn >=400 statusCode responses into exceptions
             error = Exception()
             error.status_code = resp.status_code
